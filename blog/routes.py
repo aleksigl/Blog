aaa3399f7d9a2f1d6491bad1b0600285f745fd59
aleksigl <112393919+aleksigl@users.noreply.github.com>
2025-04-16 21:30:47 +0200

    Refactor edit_entry and create_entry functions

--- blog/routes.py
+++ blog/routes.py
@@ -12,47 +12,46 @@ def index():
     return render_template("homepage.html", all_posts=all_posts)
 
 
-@app.route("/new-post/", methods=["GET", "POST"])
-def create_entry():
-    form = EntryForm()
+def manage_entry(entry_id=None):
+    entry = None
     error = ""
+    if entry_id:
+        entry = Entry.query.get_or_404(entry_id)
+
+    form = EntryForm(obj=entry)
+
     if request.method == "POST":
         if form.validate_on_submit():
             try:
-                entry = Entry(
-                    title=form.title.data,
-                    body=form.body.data,
-                    is_published=form.is_published.data
-                )
-                db.session.add(entry)
+                if entry:
+                    form.populate_obj(entry)
+                    flash('Twoje zmiany zostały opublikowane.', 'success')
+                else:
+                    entry = Entry(
+                        title=form.title.data,
+                        body=form.body.data,
+                        is_published=form.is_published.data
+                    )
+                    db.session.add(entry)
+                    flash('Twój post został opublikowany.', 'success')
                 db.session.commit()
-                flash('Twój post został opublikowany.', 'success')
                 return redirect(url_for("index"))
             except Exception as e:
                 db.session.rollback()
-                error = str(e)
+                error = f"Błąd: {str(e)}"
+                flash(error, 'danger')
     entries = Entry.query.all()
     return render_template("entry_form.html", form=form, entries=entries, error=error)
+
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+    return manage_entry()
 
 
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
 def edit_entry(entry_id):
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
-    form = EntryForm(obj=entry)
-    print(f"Title: {form.title}")
-    error = ""
-    if request.method == "POST":
-        if form.validate_on_submit():
-            try:
-                form.populate_obj(entry)
-                db.session.commit()
-                flash('Twoje zmiany zostały opublikowane.', 'success')
-                return redirect(url_for("index"))
-            except Exception as e:
-                db.session.rollback()
-                error = str(e)
-    entries = Entry.query.all()
-    return render_template("entry_form.html", form=form, entries=entries, error=error)
+    return manage_entry(entry_id)
 
 
 if __name__ == "__main__":
