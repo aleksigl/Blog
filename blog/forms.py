2a6bd9de3f7f8665c7f9b1cdcd7fa392d0598cc5
aleksigl <112393919+aleksigl@users.noreply.github.com>
2025-04-17 18:35:22 +0200

    Create login and drafts

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,27 @@
+from werkzeug.routing import ValidationError
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField
+from wtforms import StringField, TextAreaField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, Length
+from config import Config
 
 
 class EntryForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(max=100)])
     body = TextAreaField('Body', validators=[DataRequired()])
     is_published = BooleanField('Is Published', default=True)
+
+
+class LoginForm(FlaskForm):
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    def validate_username(self, field):
+        if field.data != Config.ADMIN_USERNAME:
+            raise ValidationError("Invalid username")
+        return field.data
+
+    def validate_password(self, field):
+        if field.data != Config.ADMIN_PASSWORD:
+            raise ValidationError("Invalid password")
+        return field.data
--- blog/routes.py
+++ blog/routes.py
@@ -1,15 +1,20 @@
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for, session
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
+
 
 app.config["SECRET_KEY"] = "qwerty"
 
 
-@app.route("/")
-def index():
-    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
-    return render_template("homepage.html", all_posts=all_posts)
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get('logged_in'):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return check_permissions
 
 
 def manage_entry(entry_id=None):
@@ -44,14 +49,53 @@ def manage_entry(entry_id=None):
     return render_template("entry_form.html", form=form, entries=entries, error=error)
 
 
+@app.route("/")
+def index():
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    return render_template("homepage.html", all_posts=all_posts)
+
+
 @app.route("/new-post/", methods=["GET", "POST"])
+@login_required
 def create_entry():
     return manage_entry()
 
 
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def edit_entry(entry_id):
     return manage_entry(entry_id)
+
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get('next')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            session['logged_in'] = True
+            session.permanent = True
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            errors = form.errors
+    return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        flash('You are now logged out.', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route('/drafts/', method=['GET'])
+@login_required
+def list_drafts():
+    drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+    return render_template("homepage.html", drafts=drafts)
 
 
 if __name__ == "__main__":
